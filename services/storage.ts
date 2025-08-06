63805a49d7fa7601d520c0008d612c2c2577c630
M Rifan Ardiansyah <rifansyah@Ms-MacBook-Pro.local>
2025-08-06 21:52:55 +0700

    add mmkv and abstraction

---
+++ services/storage.ts
@@ -0,0 +1,16 @@
+import { MMKV } from "react-native-mmkv";
+
+export const storage = new MMKV();
+
+export const set = (key: string, value: any) => {
+  storage.set(key, JSON.stringify(value));
+};
+
+export const get = <T = string>(key: string): T | undefined => {
+  const value = storage.getString(key);
+  return value ? (JSON.parse(value) as T) : undefined;
+};
+
+export const remove = (key: string) => {
+  storage.delete(key);
+};

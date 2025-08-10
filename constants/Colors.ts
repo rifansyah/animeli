640c8f8a3e0180c3863408cc8828014bbfceed85
M Rifan Ardiansyah <rifansyah@Ms-MacBook-Pro.local>
2025-08-10 14:36:03 +0700

    core: update colors constant

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,9 @@
-const tintColorLight = "#0a7ea4";
-
 export const Colors = {
-  light: {
-    primary: "#0891b2",
-
-    black: "#000000",
-    white: "#ffffff",
-    grey: "#808080",
-
-    text: "#11181C",
-    background: "#fff",
-    tint: tintColorLight,
-    icon: "#687076",
-    tabIconDefault: "#687076",
-    tabIconSelected: tintColorLight,
-  },
-  dark: {},
+  primary: "#3ab302",
+  black: "#000000",
+  white: "#ffffff",
+  grey: "#808080",
+  grey2: "#dedede",
+  transparent: "transparent",
+  red: "#ff0000",
 };
